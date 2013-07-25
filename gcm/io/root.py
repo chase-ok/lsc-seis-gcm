6c4f757f6b5dc8cea16074a39e1de4460cada01c
Chase Kernan <chase.kernan@students.olin.edu>
2013-07-25 17:42:03 -0400

    Fix

--- gcm/io/root.py
+++ gcm/io/root.py
@@ -3,26 +3,24 @@ from rootpy.io import root_open
 from gcm import structs
 import numpy as np
 
+_ATTR_MAP = dict(time='time',
+                 time_min='tstart', time_max='tend',
+                 freq='frequency',
+                 freq_min='fstart', freq_max='fend',
+                 amplitude='amplitude',
+                 snr='snr',
+                 q='q')
+
 def read_triggers(path):
     with root_open(path) as f:
         tree = f.triggers
         tree.create_buffer()
         buffer = tree._buffer
         
-        triggers = np.empty(len(tree), dtype=structs.trigger)
+        triggers = np.empty(len(tree), dtype=structs.trigger)        
         for i, _ in enumerate(tree):
-            print buffer['time'].value
-        
-        for trigger, _ in zip(triggers, tree):
-            # need to also iterate the tree, hence the zip
-            trigger['time'] = buffer['time']
-            trigger['time_min'] = buffer['tstart']
-            trigger['time_max'] = buffer['tend']
-            trigger['freq'] = buffer['frequency']
-            trigger['freq_min'] = buffer['fstart']
-            trigger['freq_max'] = buffer['fend']
-            trigger['amplitude'] = buffer['amplitude']
-            trigger['snr'] = buffer['snr']
-            trigger['q'] = buffer['q']
+            # need to iterate the tree, hence the _ enumerate result
+            for attr, mapped in _ATTR_MAP.iteritems():
+                triggers[i][attr] = buffer[mapped].value
     
     return triggers
