f285e2cf3abb0d3010c9793ab0a4f54e2b42ba94
Chase Kernan <chase.kernan@students.olin.edu>
2013-07-25 16:57:52 -0400

    Fix

--- gcm/io/root.py
+++ gcm/io/root.py
@@ -6,16 +6,15 @@ import numpy as np
 def read_triggers(path):
     with root_open(path) as f:
         triggers = np.empty(len(f.triggers), dtype=structs.trigger)
-        print list(f.triggers)
-        for row, raw in enumerate(f.triggers):
-            triggers[row]['time'] = raw.time
-            triggers[row]['time_min'] = raw.tstart
-            triggers[row]['time_max'] = raw.tend
-            triggers[row]['freq'] = raw.frequency
-            triggers[row]['freq_min'] = raw.fstart
-            triggers[row]['freq_max'] = raw.fend
-            triggers[row]['amplitude'] = raw.amplitude
-            triggers[row]['snr'] = raw.snr
-            triggers[row]['q'] = raw.q
+        for trigger, raw in zip(triggers, f.triggers):
+            trigger['time'] = raw.time
+            trigger['time_min'] = raw.tstart
+            trigger['time_max'] = raw.tend
+            trigger['freq'] = raw.frequency
+            trigger['freq_min'] = raw.fstart
+            trigger['freq_max'] = raw.fend
+            trigger['amplitude'] = raw.amplitude
+            trigger['snr'] = raw.snr
+            trigger['q'] = raw.q
     
     return triggers
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from gcm.io import root
 from gcm.coinc import find_coincidences
 
 def test_coinc():
-    path = "/home/chase.kernan/H1:HPI-HAM3_SENSCOR_X_FIR_IN1_DQ_1055900080_704.root"
-    triggers = root.read_triggers(path)
-    return find_coincidences(triggers, triggers)
+    base = "/home/scott.dossa/omicron/triggers/H1/1058400000_1058500000"
+    triggers1 = root.read_triggers(base + "H1:ISI-ETMY_ST1_BLND_Y_T240_CUR_IN1_DQ")
+    triggers2 = root.read_triggers(base + "H1:ISI-ETMY_ST2_BLND_Y_GS13_CUR_IN1_DQ")
+    return triggers1, triggers2, find_coincidences(triggers1, triggers2)
