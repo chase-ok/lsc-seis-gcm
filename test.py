b23f0e7a8e4b7dc79a1fadc64435dc229970e78f
Chase Kernan <chase.kernan@students.olin.edu>
2013-07-25 17:02:14 -0400

    Fix

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from gcm.io import root
 from gcm.coinc import find_coincidences
 
 def test_coinc():
-    base = "/home/scott.dossa/omicron/triggers/H1/1058400000_1058500000"
-    triggers1 = root.read_triggers(base + "H1:ISI-ETMY_ST1_BLND_Y_T240_CUR_IN1_DQ")
-    triggers2 = root.read_triggers(base + "H1:ISI-ETMY_ST2_BLND_Y_GS13_CUR_IN1_DQ")
+    triggers1 = root.read_triggers("/home/scott.dossa/omicron/triggers/H1/1058400000_1058500000/H1:ISI-ETMY_ST1_BLND_Y_L4C_CUR_IN1_DQ/H1:ISI-ETMY_ST1_BLND_Y_L4C_CUR_IN1_DQ_1058400080_704.root")
+    triggers2 = root.read_triggers("/home/scott.dossa/omicron/triggers/H1/1058400000_1058500000/H1:ISI-ETMY_ST1_BLND_Y_T240_CUR_IN1_DQ/H1:ISI-ETMY_ST1_BLND_Y_T240_CUR_IN1_DQ_1058400080_704.root")
     return triggers1, triggers2, find_coincidences(triggers1, triggers2)
