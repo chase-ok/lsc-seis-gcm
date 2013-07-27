baadf3793864719070190709596d28fdc352a96f
Chase Kernan <chase.kernan@students.olin.edu>
2013-07-26 22:43:15 -0400

    Fix

--- gcm/web/main.py
+++ gcm/web/main.py
@@ -3,8 +3,8 @@
 import bottle
 from bottle import route, run, request, static_file, view
 
-from gcm.data import hdf5
-hdf5.use_locking = False
+#from gcm.data import hdf5
+#hdf5.use_locking = False
 
 STATIC_ROOT = '/home/chase.kernan/public_html/cgi-bin/lsc-seis-gcm/static/deploy'
 bottle.TEMPLATE_PATH.append(STATIC_ROOT + "/html/")
