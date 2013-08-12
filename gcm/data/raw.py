435b2710abd0bf3a97fa0417eeed904bf65513e1
Chase Kernan <chase.kernan@ldas-pcdev1.ligo.caltech.edu>
2013-08-11 17:30:26 -0700

    Fixed raw data

--- gcm/data/raw.py
+++ gcm/data/raw.py
@@ -31,20 +31,28 @@ def fetch_raw_data(channel, start, end):
 
 SEGLEN = np.int64(256) # should match sampling rate?
 
-def process_coinc(group, coinc, time_buffer=2):
+def process_group(group, time_buffer=2, max_duration=10):
+    with co.open_coincs(group, mode='r') as table:
+        for i, coinc in enumerate(table):
+            print i, "of", len(table)
+	    process_coinc(group, coinc, time_buffer, max_duration)
+
+def process_coinc(group, coinc, time_buffer, max_duration):
     from pylal import seriesutils
 
     start = coinc.time_min - time_buffer
-    end = coinc.time_max + time_buffer
+    end = min(start + max_duration, coinc.time_max + time_buffer)
 
     for i, channel_id in enumerate(coinc.channel_ids[:coinc.length]):
         channel = chn.get_channel(channel_id)
         raw_data = fetch_raw_data(channel, start, end)
-
-        freq_min, freq_max = map(np.float64, coinc.freq_bands[i])
+        print i, start, end, raw_data.data.length
+        
+	freq_min, freq_max = map(np.float64, coinc.freq_bands[i])
         bandpassed = seriesutils.bandpass(raw_data, freq_min, freq_max, 
                                           inplace=False)
-        _write_coinc(group, channel, coinc, raw_data, bandpassed)
+	
+	_write_coinc(group, channel, coinc, raw_data, bandpassed)
         # stride = SEGLEN/4
         # spectrum = seriesutils.compute_average_spectrum(raw_data, 
         #                                                 SEGLEN, 
@@ -63,30 +71,22 @@ def _write_coinc(group, channel, coinc, raw_data, bandpassed):
     with hdf5.write_h5(make_raw_h5_path(group)) as h5:
         h5_group = h5.require_group("channel{0.id}".format(channel))
 
-        dtype = make_dtype(raw=(raw_data.dtype, (len(raw_data),)),
-                           bandpassed=(bandpassed.dtype, (len(bandpassed),)))
+        dtype = make_dtype(raw=(np.float64, (raw_data.data.length,)),
+                           bandpassed=(np.float64, (bandpassed.data.length,)))
         data = np.empty(1, dtype=dtype)
-        data[0]['raw'] = raw_data
-        data[0]['bandpassed'] = bandpassed
+        data[0]['raw'] = raw_data.data.data
+        data[0]['bandpassed'] = bandpassed.data.data
+        
+	dataset = h5_group.create_dataset(name="coinc{0.id}".format(coinc),
+                                          data=data, chunks=(1,), 
+                                          compression='gzip', compression_opts=9,
+                                          shuffle=True)
+        dataset.attrs.start_time = raw_data.epoch
+        dataset.attrs.dt = raw_data.deltaT
 
-        h5_group.create_dataset(name="coinc{0.id}".format(coinc),
-                                data=data)
-
-
-
-def add_raw_data(group, channel, coinc, start_time, frame_data):
-    with hdf5.write_h5(make_raw_h5_path(group)) as h5:
-        channel_group = h5.require_group(str(channel.id))
-        table = channel_group.require_dataset(name=str(coinc.id),
-                                              data=frame_data)
-        table.attrs.start_time = start_time
-        table.attrs.sample_rate = int(1.0/frame_data.metadata.dt)
-
-        group.require_dataset(name=self.name,
-                                            shape=(self.initial_size,),
-                                            dtype=self.dtype,
-                                            chunks=(self.chunk_size,),
-                                            maxshape=(None,),
-                                            compression=self.compression,
-                                            fletcher32=True,
-                                            exact=False)
+if __name__ == '__main__':
+    import sys
+    from gcm.data import channels
+    
+    group_id = int(sys.argv[1])
+    process_group(channels.get_group(group_id))
