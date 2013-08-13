8653c18194318424e62236c6b1921205aad2a35e
Chase Kernan <chase.kernan@ldas-pcdev1.ligo.caltech.edu>
2013-08-13 14:54:13 -0700

    Merge

--- gcm/data/raw.py
+++ gcm/data/raw.py
@@ -8,40 +8,6 @@ from random import randint
 
 RAW_DIR = "raw/"
 def make_raw_h5_path(group):
-<<<<<<< HEAD
-    name = "{0.name}.h5".format(group)
-    return make_data_path(join(RAW_DIR, name))
-
-def find_raw_data_for_coincs(group):
-    coincs = co.coincs_to_list(group)
-
-    with hdf5.write_h5(make_raw_h5_path(group)) as h5:
-        for coinc in coincs:
-            for channel_id in 
-
-def add_raw_data(group, channel, coinc, start_time, frame_data):
-    with hdf5.write_h5(make_raw_h5_path(group)) as h5:
-        channel_group = h5.require_group(str(channel.id))
-        table = channel_group.require_dataset(name=str(coinc.id),
-                                              data=frame_data)
-        table.attrs.start_time = start_time
-        table.attrs.sample_rate = int(1.0/frame_data.metadata.dt)
-
-        group.require_dataset(name=self.name,
-                                            shape=(self.initial_size,),
-                                            dtype=self.dtype,
-                                            chunks=(self.chunk_size,),
-                                            maxshape=(None,),
-                                            compression=self.compression,
-                                            fletcher32=True,
-                                            exact=False)
-
-                           
-trigger_table = hdf5.GenericTable("triggers",
-                                  dtype=trigger_dtype,
-                                  chunk_size=2**10,
-                                  initial_size=2**14)
-=======
     return make_data_path(join(RAW_DIR, "{0.name}.h5".format(group)))
 
 IFO_TO_FRAMETYPE = {'H1': 'H1_R', 'L1': 'R'}
@@ -153,4 +119,3 @@ if __name__ == '__main__':
     
     group_id = int(sys.argv[1])
     process_group(channels.get_group(group_id))
->>>>>>> b46c63ab2fb2a1f0aba17412432f259090f78b30
